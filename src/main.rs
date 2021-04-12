30f54d8d152407323f8f22a18d6a11ca4cbf6e06
Douman <douman@gmx.se>
2021-04-12 10:06:51 +0900

    Move run function to lib

--- src/main.rs
+++ src/main.rs
@@ -49,32 +49,3 @@ fn rust_main(args: c_ffi::Args) -> u8 {
         }
     }
 }
-
-fn run(args: cli::Cli, db: &std::path::Path) -> u8 {
-    let db = match db::Db::open(db) {
-        Ok(db) => db,
-        Err(error) => {
-            eprintln!("Unable to open database: {}", error);
-            return 1;
-        }
-    };
-
-    let assets = assets::Assets::new();
-
-    let rt = match tokio::runtime::Builder::new_current_thread().enable_io().enable_time().worker_threads(8).build() {
-        Ok(rt) => rt,
-        Err(error) => {
-            eprintln!("Unable to start IO loop: {}", error);
-            return 1;
-        }
-    };
-
-    let discord = match rt.block_on(discord::Discord::new(args, db.view(), assets)) {
-        Ok(discord) => discord,
-        Err(code) => return code,
-    };
-
-    rt.block_on(discord.start());
-
-    0
-}
